07df6eeba8864152473866b175064c714d35ae93
Angel Barrientos <uetiko@gmail.com>
2013-01-02 09:00:37 -0600

    templates para crear usuarios sin validaciones de campo.

---
+++ public/js/admin/admin.js
@@ -0,0 +1,36 @@
+function initEvents(){
+    registra();
+}
+
+function registra(){
+    $('#creausuario').click(function(){
+        var registro = {
+            nombre      : $('#nombre').val(),
+            apellido    : $('#apellido').val(),
+            usuario     : $('#usuario').val(),
+            password    : $('#emal').val(),
+            telefono    : $('#telefono').val(),
+            direccion   : $('#direccion').val(),
+            pais        : $('#pais').val(),
+            estado      : $('#estado').val(),
+            tipo        : $('select#tipo option:selected').val()
+        }
+        registro = JSON.stringify(registro);
+        $.ajax({
+            url : 'http://tienda.local.mx/cgi.php',
+            dataType : 'json',
+            type : 'POST',
+            data : {
+                peticion : true,
+                action : 'adminReistro',
+                json : registro
+            },
+            success : function(response, textStatus, jqXHR){
+                alert(textStatus);
+            },
+            error : function(response, textStatus, jqXHR){
+                alert(textStatus);
+            }
+        });
+    });
+}
---
+++ public/js/empleado/empleado.js
@@ -0,0 +1,35 @@
+function initEvents(){
+    registra();
+}
+
+function registra(){
+    $('#creausuario').click(function(){
+        var registro = {
+            nombre      : $('#nombre').val(),
+            apellido    : $('#apellido').val(),
+            usuario     : $('#usuario').val(),
+            password    : $('#emal').val(),
+            telefono    : $('#telefono').val(),
+            direccion   : $('#direccion').val(),
+            pais        : $('#pais').val(),
+            estado      : $('#estado').val()
+        }
+        registro = JSON.stringify(registro);
+        $.ajax({
+            url : 'http://tienda.local.mx/cgi.php',
+            dataType : 'json',
+            type : 'POST',
+            data : {
+                peticion : true,
+                action : 'empleadoReistro',
+                json : registro
+            },
+            success : function(response, textStatus, jqXHR){
+                alert(textStatus);
+            },
+            error : function(response, textStatus, jqXHR){
+                alert(textStatus);
+            }
+        });
+    });
+}
